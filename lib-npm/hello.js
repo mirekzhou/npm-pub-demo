e3e79ddc6a58c5a11f0c98870d081881e1b02fca
zhou mirek <mirek.zhou@gmail.com>
2019-12-13 13:32:18 +0800

    ~~

---
+++ lib-npm/hello.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Hello = function Hello() {
+  return _react["default"].createElement("h1", null, "Hello\u7EC4\u4EF6");
+};
+
+var _default = Hello;
+exports["default"] = _default;
