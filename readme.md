2aa4c6919491073d0a09dabded3d2e8488acb5ea
zhou mirek <mirek.zhou@gmail.com>
2019-12-13 13:07:32 +0800

    ~~

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 ## 参考文档： https://juejin.im/post/5c52a6fbe51d452d7b70f049
+
+## Demo地址： https://mirekzhou.github.io/npm-pub-demo/
--- src/app.js
+++ src/app.js
@@ -2,5 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import Hello from '../lib/hello';
 
-const App = () => <Hello />;
+const App = () => (
+    <div>
+        <h1>这是我的App头部</h1>
+        <Hello></Hello>
+    </div>
+);
+
 render(<App />, document.getElementById('root'));
